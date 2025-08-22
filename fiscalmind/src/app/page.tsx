1831ece8e88a07f033f61eb3ec80832f74ab69a5
1XMKY <imkyinnovations@gmail.com>
2025-08-22 11:35:39 -0600

    mensaje

--- fiscalmind/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white text-4xl font-bold">
-      🚀 FiscalMind Base Lista
-    </div>
-  );
-}
